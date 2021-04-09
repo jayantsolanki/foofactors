4b56bf59ca132c4103bc0e4321f1db0e34b6349b
Jayant Solanki <jayantjnp@gmail.com>
2021-04-09 15:03:56 -0700

    second r file added, test folder added

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,4 +1,20 @@
 #deployment, loading or rebuilding the package
+
+#checking the package
+devtools::check()
+
 #loading the package
 # RStudio exposes load_all() in the Build menu, in the Build pane via More > Load All
 devtools::load_all() #allows much faster iteration than actually building, installing, and attaching the package.
+
+#load all documentation in rmd
+# RStudio exposes document() in the Build menu, in the Build pane via More > Document
+devtools::document()
+
+#installing the package into the library
+# RStudio exposes similar functionality in the Build menu and in the Build pane via Install and Restart.
+devtools::install()
+
+#testing the package
+# RStudio exposes test() in the Build menu, in the Build pane via More > Test package
+devtools::test()
--- dev/dev.R
+++ dev/dev.R
@@ -1,8 +1,22 @@
-# below line is for creating the package directory, commented out
+# courtsy: https://r-pkgs.org/
+# below lines are for creating the package directory, commented out
+# run only when recreating from scratch
 # usethis::create_package("C:\\codingPractice\\foofactors")
 # using it
+
+#add license
+usethis::use_mit_license("Jane Doe")
 usethis::use_git()
+#setting up testfolder
+usethis::use_testthat()
+
+#external dependencies
+usethis::use_package("forcats")
+
 
 #creating first R file
 usethis::use_r("fbind")
+#create test file for above folder
+usethis::use_test("fbind")
+
 
